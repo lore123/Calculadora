7755d20ccfaee7b2f38902a658161047e710da3d
Hedi1993 <elhedii@hotmail.com>
2015-08-08 11:49:59 -0500

    clase Resta

---
+++ Clase_Resta.java
@@ -0,0 +1,17 @@
+
+public class Clase_Resta {
+    public static int res;
+    
+    public static void resta(int x,int y)
+    {
+       res=x-y;
+       
+        System.out.println("el resultado de la suma es :"+res);
+    }
+    
+    public static void main(String[]args)
+    {
+    resta(4,6);    
+    }
+            
+}
